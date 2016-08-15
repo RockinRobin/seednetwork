f8a41a09772b3c3651d9d0d5e3004db477147847
mpubatman <RockinRobin@users.noreply.github.com>
2016-08-14 22:09:26 -0400

    Fix url pattern depreciation upon django update

--- seedlibrary/urls.py
+++ seedlibrary/urls.py
@@ -2,17 +2,17 @@ from django.conf.urls import patterns, include, url
 
 from seedlibrary import views, views_search
 
-urlpatterns = patterns('',
-	url(r'^$', 'seedlibrary.views.home', name='home'),
-	url(r'^seeds/create/$', 'seedlibrary.views.seed_create'),
-	url(r'^seeds/create-confirm/$', 'seedlibrary.views.seed_create_confirm'),
-	url(r'^seeds/$', 'seedlibrary.views.seeds'),
-	url(r'^seeds/export/$', 'seedlibrary.views.seed_export'),
-	url(r'^seeds/edit/(?P<id>[0-9]+)$', 'seedlibrary.views.seed_edit'),
-	url(r'^seeds/confirm-archive/(?P<id>[0-9]+)$', 'seedlibrary.views.seed_confirm_archive'),
-	url(r'^search/$', 'seedlibrary.views_search.seed_search'),
-	url(r'^events/$', 'seedlibrary.views.events'),
-	url(r'^seeds-at-event/(?P<id>[0-9]+)$', 'seedlibrary.views.seeds_at_event')
-)
+urlpatterns = [
+	url(r'^$', views.home, name='home'),
+	url(r'^seeds/create/$', views.seed_create),
+	url(r'^seeds/create-confirm/$', views.seed_create_confirm),
+	url(r'^seeds/$', views.seeds),
+	url(r'^seeds/export/$', views.seed_export),
+	url(r'^seeds/edit/(?P<id>[0-9]+)$', views.seed_edit),
+	url(r'^seeds/confirm-archive/(?P<id>[0-9]+)$', views.seed_confirm_archive),
+	url(r'^search/$', views_search.seed_search),
+	url(r'^events/$', views.events),
+	url(r'^seeds-at-event/(?P<id>[0-9]+)$', views.seeds_at_event),
+]
 
 
--- seednetwork/urls.py
+++ seednetwork/urls.py
@@ -1,18 +1,20 @@
 from django.conf.urls import patterns, include, url
+import django.contrib.auth.views
 
 # Uncomment the next two lines to enable the admin:
-from django.contrib import admin
-from seednetwork import settings
+from django.contrib import admin, auth
+from seednetwork import settings, views, views_user
+from django.views  import static
 from seednetwork.forms import SeedNetworkAuthForm, SeedNetworkPasswordChangeForm
 
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = [
     # Examples:
     # url(r'^$', 'seednetwork.views.home', name='home'),
     url(r'^seedlibrary/', include('seedlibrary.urls')),
 
-    url(r'^$', 'seednetwork.views.home'),
+    url(r'^$', views.home),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
@@ -21,38 +23,38 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 
     # user management
-	(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name':'login.html', 'authentication_form':SeedNetworkAuthForm}),
-	(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'template_name':'logout.html', 'next_page':'/'}),
-	(r'^accounts/profile/$', 'seednetwork.views_user.profile'),
-	(r'^accounts/member/(?P<mid>[0-9]+)$', 'seednetwork.views_user.member'),
-	(r'^accounts/members/$', 'seednetwork.views_user.members'),
+    url(r'^accounts/login/$', auth.views.login, {'template_name':'login.html', 'authentication_form':SeedNetworkAuthForm}),
+    url(r'^accounts/logout/$', auth.views.logout, {'template_name':'logout.html', 'next_page':'/'}),
+    url(r'^accounts/profile/$', views_user.profile),
+    url(r'^accounts/member/(?P<mid>[0-9]+)$', views_user.member),
+    url(r'^accounts/members/$', views_user.members),
 
-	(r'^accounts/profile-edit/$', 'seednetwork.views_user.edit_profile'),
+    url(r'^accounts/profile-edit/$', views_user.edit_profile),
 
-    (r'^accounts/new-user/$', 'seednetwork.views_user.new_user'),
+    url(r'^accounts/new-user/$', views_user.new_user),
 
 
-	(r'^accounts/reset-password/$', 'django.contrib.auth.views.password_reset',
+    url(r'^accounts/reset-password/$', auth.views.password_reset,
 		 {'template_name':'password_reset.html',
 		  'email_template_name':'password_reset_email.html'}),
 
-	(r'^accounts/reset-password/done/$', 'django.contrib.auth.views.password_reset_done',
+    url(r'^accounts/reset-password/done/$', auth.views.password_reset_done,
 		 {'template_name':'password_reset_done.html'}),
 
-	(r'^accounts/reset-password-confirm/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$',
-	 'django.contrib.auth.views.password_reset_confirm', {'template_name':'password_reset_confirm.html'}),
+    url(r'^accounts/reset-password-confirm/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$',
+	 auth.views.password_reset_confirm, {'template_name':'password_reset_confirm.html'}),
 
-	(r'^accounts/reset-password-complete/$', 'django.contrib.auth.views.password_reset_complete',
+    url(r'^accounts/reset-password-complete/$', auth.views.password_reset_complete,
 		{'template_name':'password_reset_complete.html'}),
 
-	(r'^accounts/change-password/$', 'django.contrib.auth.views.password_change',
+    url(r'^accounts/change-password/$', auth.views.password_change,
 		 {'template_name':'password_change.html', 'password_change_form':SeedNetworkPasswordChangeForm}),
 
-	(r'^accounts/change-password/done/$', 'django.contrib.auth.views.password_change_done',
+    url(r'^accounts/change-password/done/$', auth.views.password_change_done,
 		 {'template_name':'password_change_done.html'}),
-)
+]
 
 if not settings.DEBUG:
-	urlpatterns += patterns('',
-		(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
-	                        )
+	urlpatterns += [
+	    url(r'^static/(?P<path>.*)$', static.serve, {'document_root': settings.STATIC_ROOT}),
+	               ]
