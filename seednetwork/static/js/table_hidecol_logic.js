02cb75d170e5193286d735abbbdde3e23efbd901
RockinRobin <RockinRobin@users.noreply.github.com>
2017-04-09 19:55:14 -0400

    Hide actions column on search results
    Also correct location of js folder
    TG-49

---
+++ seednetwork/static/js/table_hidecol_logic.js
@@ -0,0 +1,14 @@
+if (document.URL.indexOf("search")>=0) {
+    hide_table_actions();
+}
+
+function hide_table_actions(){
+
+   var table = document.querySelector( '.results');
+   var cellIndex = table.rows[0].cells.length - 1;  // index for last column
+   var num_columns = table.rows[0].cells.length;
+   for (i = 0; i<table.rows.length; i++){
+        table.rows[i].cells[cellIndex % num_columns].style.display = 'none';
+    }
+
+}
