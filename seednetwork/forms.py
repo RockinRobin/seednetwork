f6d787865953d4f48ed6c9222870581f01257600
RockinRobin <RockinRobin@users.noreply.github.com>
2017-09-08 15:54:34 -0400

    Clean url field
    TG-103

--- seednetwork/forms.py
+++ seednetwork/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm, UserCreationForm
+from django.forms.widgets import TextInput
 from localflavor.us.forms import USPhoneNumberField, USStateField, USZipCodeField, USStateSelect
 from generic.forms import CountryField, CountrySelect  
 
@@ -72,7 +73,7 @@ class MemberInfoForm(SeedNetworkBaseForm):
 	mailing_address_is_public = forms.BooleanField(label = "Mailing address is visible to members", required=False, initial=True)
 
 	about_me = forms.CharField(widget=forms.Textarea(attrs={'rows':'5', 'cols':'60'}), required=False, help_text="Describe your work, interests, projects, growing conditions, etc.  Also indicate under what conditions you would share seeds. These details might include sharing, trading, purchase, shipping and payment information.")
-	external_url=forms.URLField(label="External URL", required=False, help_text="Include an external webpage here, if desired.")
+	external_url=forms.URLField(label="External URL", required=False, help_text="Include an external webpage here, if desired.",initial="http://",widget=TextInput)
 	include_in_member_profiles = forms.BooleanField(label="Include in member index", required=False, initial=True)
 
 	def clean(self):
